adfaf11d13803add4e229c875233190dc6e711c8
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-11 20:32:54 -0800

    Update HKDF to use new MAC api and started adding testing

--- src/lib.rs
+++ src/lib.rs
@@ -13,27 +13,46 @@ pub struct Hkdf<const BLOCK_SIZE: usize, const HASH_SIZE: usize, H: HashFn<BLOCK
 impl<const BLOCK_SIZE: usize, const HASH_SIZE: usize, H: HashFn<BLOCK_SIZE, HASH_SIZE>> Kdf
     for Hkdf<BLOCK_SIZE, HASH_SIZE, H>
 {
-    fn kdf<const KEY_SIZE: usize>(km: &[u8], salt: &[u8], info: &[u8]) -> Key<KEY_SIZE> {
-        let mut key = [0u8; KEY_SIZE];
-
-        let prk = Hmac::<BLOCK_SIZE, HASH_SIZE, H>::compute(km, salt);
+    fn kdf(ikm: &[u8], salt: &[u8], info: &[u8], okm: &mut [u8]) {
+        let prk = Hmac::<BLOCK_SIZE, HASH_SIZE, H>::mac(ikm, salt);
+        let mut mac = Hmac::<BLOCK_SIZE, HASH_SIZE, H>::new(&prk);
 
         let mut t = [0u8; HASH_SIZE];
-        let mut l = 0;
+        let mut written = 0;
 
-        for i in 0..(KEY_SIZE / HASH_SIZE) + 1 {
-            t = Hmac::<BLOCK_SIZE, HASH_SIZE, H>::compute(&t, &prk);
+        let n = (okm.len() / HASH_SIZE) + (okm.len() % HASH_SIZE != 0) as usize;
 
-            for i in t {
-                if l >= key.len() {
-                    break;
-                }
+        for i in 1..n + 1 {
+            mac.update(&t);
+            mac.update(info);
+            mac.update(&[(i as u8)]);
 
-                key[l] = i;
-                l += 1;
-            }
+            t = mac.finalize_reset();
+
+            let to_write = if okm.len() - written > HASH_SIZE {
+                HASH_SIZE
+            } else {
+                okm.len() - written
+            };
+
+            okm[written..(written + to_write)].copy_from_slice(&t[..to_write]);
+            written += to_write;
         }
+    }
+}
 
-        key
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use hex::ToHex;
+    use librypt_hash_md5::Md5;
+
+    #[test]
+    fn test_hkdf() {
+        let mut key = [0u8; 32];
+        Hkdf::<64, 16, Md5>::kdf(b"test", b"test", b"test", &mut key);
+
+        println!("Key: {}", key.encode_hex::<String>());
     }
 }
