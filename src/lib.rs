f637af3e78e7769dc36ba4265cef7978ea360dc6
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-11 22:16:10 -0800

    Fixed HKDF

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::marker::PhantomData;
 
 use librypt_hash::HashFn;
-use librypt_kdf::{Kdf, Key};
+use librypt_kdf::Kdf;
 use librypt_mac::MacFn;
 use librypt_mac_hmac::Hmac;
 
@@ -14,13 +14,11 @@ impl<const BLOCK_SIZE: usize, const HASH_SIZE: usize, H: HashFn<BLOCK_SIZE, HASH
     for Hkdf<BLOCK_SIZE, HASH_SIZE, H>
 {
     fn kdf(ikm: &[u8], salt: &[u8], info: &[u8], okm: &mut [u8]) {
-        let salt = if salt.len() > 0 {
-            salt
-        } else {
-            &[0u8; HASH_SIZE]
-        };
+        let salt = (salt.len() > 0)
+            .then_some(salt)
+            .unwrap_or(&[0u8; HASH_SIZE]);
 
-        let prk = Hmac::<BLOCK_SIZE, HASH_SIZE, H>::mac(ikm, salt);
+        let prk = Hmac::<BLOCK_SIZE, HASH_SIZE, H>::mac(salt, ikm);
         let mut mac = Hmac::<BLOCK_SIZE, HASH_SIZE, H>::new(&prk);
 
         let mut t = [0u8; HASH_SIZE];
@@ -29,7 +27,7 @@ impl<const BLOCK_SIZE: usize, const HASH_SIZE: usize, H: HashFn<BLOCK_SIZE, HASH
         let n = (okm.len() / HASH_SIZE) + (okm.len() % HASH_SIZE != 0) as usize;
 
         for i in 1..n + 1 {
-            mac.update(&t);
+            mac.update((i > 1).then_some(&t[..]).unwrap_or(&[]));
             mac.update(info);
             mac.update(&[(i as u8)]);
 
@@ -59,6 +57,9 @@ mod tests {
         let mut key = [0u8; 32];
         Hkdf::<64, 16, Md5>::kdf(b"test", b"test", b"test", &mut key);
 
-        println!("Key: {}", key.encode_hex::<String>());
+        assert_eq!(
+            key.encode_hex::<String>(),
+            "62375f121c8aab9a2cf3a7fc48bef9e1db7efb594c7ce323781121e5b63898b5"
+        );
     }
 }
